950041b41aabe61387e766d0152ff1c71d2856fe
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2023-10-08 20:34:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ from env_variables import *
 
 app = Flask(__name__)
 
+parts = DATABASE_URL.split("://")
+protocol = parts[0]
+
+user_pass, rest = parts[1].split("@")
+postgres_user, postgres_pass = user_pass.split(":")
+
+url_port_db, _ = rest.split("?")
+postgres_url, port_db = url_port_db.split(":")
+postgres_port, postgres_database = port_db.split("/")
+
+# Criar a engine
 engine = create_engine(f"postgresql://{postgres_user}:{postgres_pass}@{postgres_url}:{postgres_port}/{postgres_database}")
 
 @app.route('/execute_sql', methods=['POST'])
@@ -16,7 +27,7 @@ def execute_sql():
     query = data['query']
 
     if not query.lower().startswith('select'):
-        return jsonify({'error': 'Only SELECT queries are allowed'}), 400
+       return jsonify({'error': 'Only SELECT queries are allowed'}), 400
 
     try:
         df = pd.read_sql(query, engine)
